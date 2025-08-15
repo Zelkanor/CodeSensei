444ec128cd5d1af4a3e55f67acc7991cc05ef9d0
Nikhil Krishna <nikhil.krishnakn@gmail.com>
2025-08-15 15:59:13 +0530

    Assets and MetaData added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl">Welcome to Next.js</h1>
+      <h1 className="text-3xl font-inter">Welcome to Next.js</h1>
     </div>
   );
 };
