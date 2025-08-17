7d60e8fe9ddf37334450218b215e28adfa864e9d
Nikhil Krishna <nikhil.krishnakn@gmail.com>
2025-08-17 07:11:52 +0530

    Mobile Nav and Nav Links Implemented

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,18 @@
+"use client";
+
 import React from "react";
 
+import AuthForm from "@/components/forms/AuthForm";
+import { LoginSchema } from "@/lib/validation";
+
 const Login = () => {
-  return <div>LOGIN</div>;
+  return (
+    <AuthForm
+      formType="LOGIN"
+      schema={LoginSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 export default Login;
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import React from "react";
 
+import AuthForm from "@/components/forms/AuthForm";
+import { RegisterSchema } from "@/lib/validation";
+
 const Register = () => {
-  return <div></div>;
+  return (
+    <AuthForm
+      formType="REGISTER"
+      schema={RegisterSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default Register;
